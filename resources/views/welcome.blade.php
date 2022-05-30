f82523c76845a8a9ad94c1e82869b96fb2fc525e
richard9809 <richard.ndisya@strathmore.edu>
2022-05-30 13:16:04 +0300

    init
    Signed-off-by: richard9809 <richard.ndisya@strathmore.edu>

---
+++ resources/views/welcome.blade.php
@@ -0,0 +1,25 @@
+<!DOCTYPE html>
+<html lang="{{ str_replace('_', '-', app()->getLocale()) }}">
+    <head>
+        <meta charset="utf-8">
+        <meta name="viewport" content="width=device-width, initial-scale=1">
+        <!-- Icon -->
+        <link rel="icon" href="{{ asset('img/logo.png') }}">
+
+        <title>Cifrar</title>
+
+        <!-- Bootstrap CSS -->
+        <link href="https://cdn.jsdelivr.net/npm/bootstrap@5.0.1/dist/css/bootstrap.min.css" rel="stylesheet">
+        <!--  Font Awesome Icons -->
+        <link href="https://maxcdn.bootstrapcdn.com/font-awesome/4.2.0/css/font-awesome.min.css" rel="stylesheet">
+        <link href="https://stackpath.bootstrapcdn.com/bootstrap/4.0.0/css/bootstrap.min.css" rel="stylesheet" />
+        <link href="https://cdnjs.cloudflare.com/ajax/libs/font-awesome/4.7.0/css/font-awesome.css" rel="stylesheet" />
+
+    </head>
+    <body class="antialiased">
+        <nav class="navbar navbar-expand-lg navbar-light justify-content-between">
+            <img src="{{ asset('img/logo.png') }}" class="rounded justify-content-between" width="300" height="300">
+        </nav>
+       
+    </body>
+</html>
